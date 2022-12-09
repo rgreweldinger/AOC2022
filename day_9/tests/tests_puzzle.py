9f9225638cb891cc42a7aafc4e1d7e1d59c7ef60
Ronald Greweldinger <r.greweldinger@coolblue.nl>
2022-12-09 10:56:32 +0100

    [DAY-9] Advent of code 2022

--- day_9/tests/tests_puzzle.py
+++ day_9/tests/tests_puzzle.py
@@ -3,13 +3,17 @@ from src.puzzle import calc_puzzle_1, calc_puzzle_2
 
 def test_calc_1():
     with open("day_9/tests/test_input.txt", "r") as input:
-        list_input = [cookie.strip() for cookie in input]
+        test_1, test_2 = input.read().split("\n\n")
+        list_input = test_1.split("\n")
+        print(list_input)
         output = calc_puzzle_1(list_input)
-    assert output == "x"
+    assert output == 12
 
 
 def test_calc_2():
     with open("day_9/tests/test_input.txt", "r") as input:
-        list_input = [cookie.strip() for cookie in input]
+        test_1, test_2 = input.read().split("\n\n")
+        list_input = test_2.split("\n")
+        print(list_input)
         output = calc_puzzle_2(list_input)
-    assert output == "x"
+    assert output == 36
